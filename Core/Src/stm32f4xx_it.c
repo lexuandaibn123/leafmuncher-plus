681c710ff5521d4863e204a27875c223eb1db088
lexuandaibn123 <lexuandaibn@gmail.com>
2026-06-21 21:51:07 +0700

    chore(cubemx): enable TIM7 basic timer (1ms) via CubeMX regen
    Adds MX_TIM7_Init + TIM7_IRQHandler skeleton (PSC=7199, ARR=9 -> 1ms,
    NVIC enabled). Hardware prep for T017 (real ms time-base + LED heartbeat);
    ISR logic to follow.

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -59,6 +59,7 @@ extern HCD_HandleTypeDef hhcd_USB_OTG_HS;
 extern DMA_HandleTypeDef hdma_adc1;
 extern DMA2D_HandleTypeDef hdma2d;
 extern LTDC_HandleTypeDef hltdc;
+extern TIM_HandleTypeDef htim7;
 extern TIM_HandleTypeDef htim6;
 
 /* USER CODE BEGIN EV */
@@ -175,6 +176,20 @@ void TIM6_DAC_IRQHandler(void)
   /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
 
   /* USER CODE END TIM6_DAC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM7 global interrupt.
+  */
+void TIM7_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM7_IRQn 0 */
+
+  /* USER CODE END TIM7_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim7);
+  /* USER CODE BEGIN TIM7_IRQn 1 */
+
+  /* USER CODE END TIM7_IRQn 1 */
 }
 
 /**
